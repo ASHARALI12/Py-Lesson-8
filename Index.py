e1dbf7dc908db6b21413cf1c9f5faaab303ba26d
ASHARALI12 <mbrnshkr@gmail.com>
2025-03-27 19:50:57 +0500

    Python

---
+++ Index.py
@@ -0,0 +1,11 @@
+medical_cause = input("Did you have a medical cause Y Or N: ")
+atten = int(input("Enter the attendence of the student: "))
+
+if medical_cause == 'Y' :
+    print("You are allowed")
+else:
+    if atten>=75 :
+        print("Allowed")
+    else :
+        print("Not Allowed")
+
